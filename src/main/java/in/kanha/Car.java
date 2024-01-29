3fd1184baf30590057a9f044523ceb3c04147de8
Kanha Driven <kanha@Kanha-Driven.mshome.net>
2024-01-29 17:01:34 +0530

    First Commit

---
+++ src/main/java/in/kanha/Car.java
@@ -0,0 +1,12 @@
+package in.kanha;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	Car()
+	{
+		System.out.println("Constructor");
+	}
+}
---
+++ src/main/java/in/kanha/service/Service.java
@@ -0,0 +1,11 @@
+package in.kanha.service;
+
+
+@org.springframework.stereotype.Service
+public class Service {
+
+	Service()
+	{
+		System.out.println("Service :: Constructor..");
+	}
+}
